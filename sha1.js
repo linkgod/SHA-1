40807daef7d0d00fc134e5709290872fda3b0fe7
皓勋 <wangxun.wx@alibaba-inc.com>
2020-07-01 17:09:26 +0800

    For your convenience, the code comments have been translated by Google.

--- sha1.js
+++ sha1.js
@@ -1,7 +1,9 @@
+// For your convenience, the code comments have been translated by Google.
 (function () {
   var root = this;
 
   // 消息填充位，补足长度。
+  // Message padding bits, complement the length.
   function fillString(str) {
     var blockAmount = ((str.length + 8) >> 6) + 1,
       blocks = [],
@@ -20,6 +22,7 @@
   }
 
   // 将输入的二进制数组转化为十六进制的字符串。
+  // Convert the input binary array to a hexadecimal string.
   function binToHex(binArray) {
     var hexString = "0123456789abcdef",
       str = "",
@@ -34,6 +37,8 @@
   }
 
   // 核心函数，输出为长度为5的number数组，对应160位的消息摘要。
+  // The core function, the output is a number array with a length of 5,
+  // corresponding to a 160-bit message digest.
   function core(blockArray) {
     var w = [],
       a = 0x67452301,
@@ -82,6 +87,8 @@
   }
 
   // 根据t值返回相应得压缩函数中用到的f函数。
+  // According to the t value, return the corresponding f function used in
+  // the compression function.
   function ft(t, b, c, d) {
     if (t < 20) {
       return (b & c) | ((~b) & d);
@@ -95,6 +102,8 @@
   }
 
   // 根据t值返回相应得压缩函数中用到的K值。
+  // According to the t value, return the corresponding K value used in
+  // the compression function.
   function kt(t) {
     return (t < 20) ? 0x5A827999 :
       (t < 40) ? 0x6ED9EBA1 :
@@ -102,6 +111,9 @@
   }
 
   // 模2的32次方加法，因为JavaScript的number是双精度浮点数表示，所以将32位数拆成高16位和低16位分别进行相加
+  // Modulo 2 to the 32nd power addition, because JavaScript's number is a
+  // double-precision floating-point number, so the 32-bit number is split
+  // into the upper 16 bits and the lower 16 bits are added separately.
   function modPlus(x, y) {
     var low = (x & 0xFFFF) + (y & 0xFFFF),
       high = (x >> 16) + (y >> 16) + (low >> 16);
@@ -109,12 +121,17 @@
     return (high << 16) | (low & 0xFFFF);
   }
 
-  //对输入的32位的num二进制数进行循环左移 ,因为JavaScript的number是双精度浮点数表示，所以移位需需要注意
+  // 对输入的32位的num二进制数进行循环左移 ,因为JavaScript的number是双精度浮点数表示，所以移位需需要注意
+  // Rotate left of the input 32-bit num binary number, because JavaScript's
+  // number is a double-precision floating-point number, so you need to pay
+  //  attention to the shift.
   function cyclicShift(num, k) {
     return (num << k) | (num >>> (32 - k));
   }
 
   // 主函数根据输入的消息字符串计算消息摘要，返回十六进制表示的消息摘要
+  // The main function calculates the message digest based on the input message
+  // string and returns the message digest in hexadecimal.
   function sha1(s) {
     return binToHex(core(fillString(s)));
   }
